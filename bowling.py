6344cb4a400e765694628f26888f3144461983a6
Linczmayer Ádám <lincy.adam@gmail.com>
2017-07-30 21:11:20 +0200

    Remove more duplicates

--- bowling.py
+++ bowling.py
@@ -4,23 +4,25 @@ import string
 def score(game):
     result = 0
     frame = 1
+    strike = ['X', 'x']
+    spare = '/'
     in_first_half = True
     for i in range(len(game)):
-        if game[i] == '/':
+        if game[i] == spare:
             result += 10 - last
         else:
             result += get_value(game[i])
         if frame < 10 and get_value(game[i]) == 10:
-            if game[i] == '/':
+            if game[i] == spare:
                 result += get_value(game[i+1])
-            elif game[i] in ['X', 'x']:
-                result += get_value(game[i+1])  
-                if game[i+2] == '/':
+            elif game[i] in strike:
+                result += get_value(game[i+1])
+                if game[i+2] == spare:
                     result += 10 - get_value(game[i+1])
                 else:
                     result += get_value(game[i+2])
         last = get_value(game[i])
-        if not in_first_half or game[i] in ['X', 'x']:
+        if not in_first_half or game[i] in strike:
             in_first_half = True
             frame += 1
         else:
